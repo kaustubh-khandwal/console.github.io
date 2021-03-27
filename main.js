fcf300356b234b52808c330c4ee6280b937d1572
kaustubh-khandwal <76221706+kaustubh-khandwal@users.noreply.github.com>
2021-03-27 13:34:16 +0530

    kk

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("My first console project");
+
+var first_name = "Kaustubh";
+var last_name = "Khandwal";
+var full_name = first_name.concat(last_name)
+console.log(full_name)
+
+var number1 = 16 ;
+var number2 = 24 ;
+console.log(number1+number2);
+
+console.log(number1-number2);
+
+console.log(number1*number2);
+
+console.log(number1/number2);
